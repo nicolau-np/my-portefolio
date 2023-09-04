3cd851879c7753cfc0c0d42782aead46fcf56faf
nicolau-np <mr1Normaliii@gmail.com>
2023-09-04 20:33:57 +0100

    adicionando animacoes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,37 @@
 var previousScrollPosition = document.documentElement.scrollTop;
-var buttonMenu = document.querySelector('.button-menu');
-var menuMobile = document.querySelector('.menu-mobile');
+var buttonMenu = document.querySelector(".button-menu");
+var menuMobile = document.querySelector(".menu-mobile");
 
-var btnToTop = document.querySelector('.btn-to-top');
+var btnToTop = document.querySelector(".btn-to-top");
 
-buttonMenu.addEventListener('click', function(e){
-    menuMobile.classList.toggle('hidden');
+buttonMenu.addEventListener("click", function(e) {
+  menuMobile.classList.toggle("hidden");
 });
 
-
-window.addEventListener('scroll', function(e){
-    const currentScrollPosition = document.documentElement.scrollTop;
-    if(previousScrollPosition<currentScrollPosition){
-        btnToTop.classList.remove('hidden');
-    }else if(previousScrollPosition == currentScrollPosition){
-        btnToTop.classList.add('hidden');
-    }
+window.addEventListener("scroll", function(e) {
+  const currentScrollPosition = document.documentElement.scrollTop;
+  if (previousScrollPosition < currentScrollPosition) {
+    btnToTop.classList.remove("hidden");
+  } else if (previousScrollPosition == currentScrollPosition) {
+    btnToTop.classList.add("hidden");
+  }
 });
-
 
 /**animate */
-const target = document.querySelectorAll('[data-anime]');
+
+const target = document.querySelectorAll("[data-anime]");
 const animateClass = "animate";
 
-function animeScroll(){
-const windowTop = window.scrollY +((window.innerHeight*3)/4);
-target.forEach((e)=>{
-    console.log(e.offsetTop);
-    if((windowTop) > e.offsetTop){
-e.classList.add('animate');
+function animeScroll() {
+  const windowTop = window.scrollY + window.innerHeight * 3 / 4;
+
+  target.forEach(e => {
+    if (windowTop > e.offsetTop) {
+      e.classList.add("animate");
     }
-})
+  });
 }
 
-window.addEventListener('scroll', e=>{
-animeScroll();
-})
-
-
+window.addEventListener("scroll", e => {
+  animeScroll();
+});
